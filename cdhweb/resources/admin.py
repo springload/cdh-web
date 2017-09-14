ea7692449481fc31b74082f0c9ff959b8d62bc60
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-09-14 17:33:42 -0400

    Customize admin User list display to show permissions, status

--- cdhweb/resources/admin.py
+++ cdhweb/resources/admin.py
@@ -1,13 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from mezzanine.pages.admin import PageAdmin
-from .models import ResourceType, Attachment, LandingPage
+
+
+from cdhweb.resources.models import ResourceType, Attachment, LandingPage
 
 class ResourceTypeAdmin(admin.ModelAdmin):
     # TODO: drag and drop to set sort order in future
     list_display = ('name', 'sort_order')
     list_editable = ('sort_order', )
 
+# customize default User display
+class LocalUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('is_superuser', 'is_active',
+        'last_login')
+
 
 admin.site.register(ResourceType, ResourceTypeAdmin)
 admin.site.register(Attachment)
 admin.site.register(LandingPage, PageAdmin)
+# unregister and re-register User
+admin.site.unregister(User)
+admin.site.register(User, LocalUserAdmin)
+
+
+
+
