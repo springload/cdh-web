672b4bbcdf6925fea24bb22365920abd3e09de6d
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-09-22 11:39:27 -0400

    Set displayables in template context as page for common display logic

--- cdhweb/projects/views.py
+++ cdhweb/projects/views.py
@@ -2,17 +2,29 @@ from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from cdhweb.projects.models import Project
+from cdhweb.resources.views import LastModifiedMixin, LastModifiedListMixin
 
 
-class ProjectListView(ListView):
+class ProjectMixinView(object):
+    '''View mixin that sets model to Project and returns a
+    published Project queryset.'''
     model = Project
 
     def get_queryset(self):
+        # use displayable manager to find published events only
+        # (or draft profiles for logged in users with permission to view)
         return Project.objects.published() # TODO: published(for_user=self.request.user)
 
 
-class ProjectDetailView(DetailView):
-    model = Project
+class ProjectListView(ProjectMixinView, ListView, LastModifiedListMixin):
+    pass
 
-    def get_queryset(self):
-        return Project.objects.published() # TODO: published(for_user=self.request.user)
+
+class ProjectDetailView(ProjectMixinView, DetailView, LastModifiedMixin):
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProjectDetailView, self).get_context_data(*args, **kwargs)
+        # also set object as page for common page display functionality
+        context['page'] = self.object
+        return context
+
