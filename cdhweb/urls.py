4f7481f97398ef7d985618b72a6aae07043f226b
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2017-09-25 11:29:53 -0400

    Rem. stray double-import of settings from urls.py

--- cdhweb/urls.py
+++ cdhweb/urls.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 
-from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 # from django.conf.urls.static import static
