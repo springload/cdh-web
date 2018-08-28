da8e0248119dc034b2484ee6c3890f1a251c918d
rlskoeser <rebecca.s.koeser@princeton.edu>
2018-08-28 10:07:27 -0400

    Adapt url_to_icon logic to set opengraph/twitter preview image

--- cdhweb/resources/templatetags/cdh_tags.py
+++ cdhweb/resources/templatetags/cdh_tags.py
@@ -1,24 +1,40 @@
+from collections import OrderedDict
+
 from django import template
+from django.conf import settings
+
+from cdhweb.resources.utils import absolutize_url
 
 register = template.Library()
+
+#: mapping from url portion to CDH icon name; ordered to ensure more specific
+#: options are checked first.
+URL_ICON_MAPPING = OrderedDict([
+    ('/people/', 'ppl'),
+    ('/projects/', 'folder'),
+    ('/events/', 'cal'),
+    ('/contact/', 'convo'),
+    ('seed-grant', 'seed'),
+    ('fellowship', 'medal'),
+    ('prize', 'medal'),
+    ('grant', 'grant'),
+    ('funding', 'grant')
+])
+
 
 
 @register.filter
 def url_to_icon(value):
-    '''Return appropriate CDH icon name based on URL.'''
-    if '/people/' in value:
-        return 'people'
-    if '/projects/' in value:
-        return 'folder'
-    if '/events/' in value:
-        return 'cal'
-    if '/contact/' in value:
-        return 'convo'
-    if 'seed-grant' in value:
-        return 'seed'
-    if 'fellowship' in value or 'prize' in value:
-        return 'medal'
-    if 'grants' in value or 'funding' in value:
-        return 'grant'
+    '''Return an appropriate CDH icon name based on URL.'''
+    for url, icon in URL_ICON_MAPPING.items():
+        if url in value:
+            return icon
+    return ''
 
+@register.filter
+def url_to_icon_path(value):
+    '''Return absolute path to CDH icon name based on URL.'''
+    img = url_to_icon(value)
+    if img:
+        return absolutize_url('{}img/cdh-icons/{}.svg'.format(settings.STATIC_URL, img))
     return ''
--- cdhweb/resources/tests/test_tags.py
+++ cdhweb/resources/tests/test_tags.py
@@ -1,11 +1,27 @@
-from cdhweb.resources.templatetags.cdh_tags import url_to_icon
+from django.contrib.sites.models import Site
+import pytest
+
+from cdhweb.resources.templatetags import cdh_tags
 
 def test_url_to_icon():
-    assert url_to_icon('/people/staff/') == 'people'
-    assert url_to_icon('/projects/') == 'folder'
-    assert url_to_icon('/events/') == 'cal'
-    assert url_to_icon('/contact/') == 'convo'
-    assert url_to_icon('/grants/seed-grant/') == 'seed'
-    assert url_to_icon('/graduate-fellowships/') == 'medal'
-    assert url_to_icon('/grants/') == 'grant'
-    assert url_to_icon('/unknown/') == ''
+    assert cdh_tags.url_to_icon('/people/staff/') == 'ppl'
+    assert cdh_tags.url_to_icon('/projects/') == 'folder'
+    assert cdh_tags.url_to_icon('/events/') == 'cal'
+    assert cdh_tags.url_to_icon('/contact/') == 'convo'
+    assert cdh_tags.url_to_icon('/grants/seed-grant/') == 'seed'
+    assert cdh_tags.url_to_icon('/graduate-fellowships/') == 'medal'
+    assert cdh_tags.url_to_icon('/grants/') == 'grant'
+    assert cdh_tags.url_to_icon('/unknown/') == ''
+
+
+@pytest.mark.django_db
+def test_url_to_icon_path():
+    domain = Site.objects.get_current().domain
+    assert cdh_tags.url_to_icon_path('/people/staff/') == 'https://{}/static/img/cdh-icons/ppl.svg'.format(domain)
+    # assert cdh_tags.url_to_icon('/projects/') == 'folder'
+    # assert cdh_tags.url_to_icon('/events/') == 'cal'
+    # assert cdh_tags.url_to_icon('/contact/') == 'convo'
+    # assert cdh_tags.url_to_icon('/grants/seed-grant/') == 'seed'
+    # assert cdh_tags.url_to_icon('/graduate-fellowships/') == 'medal'
+    # assert cdh_tags.url_to_icon('/grants/') == 'grant'
+    # assert cdh_tags.url_to_icon('/unknown/') == ''
