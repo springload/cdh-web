43f0c40622f2ba1952d673cc6d8b7eb36e8489d3
Nick Budak <thatbudakguy@gmail.com>
2019-08-29 16:56:20 -0400

    add default ordering for Grants

--- cdhweb/projects/models.py
+++ cdhweb/projects/models.py
@@ -150,6 +150,9 @@ class Grant(DateRange):
     project = models.ForeignKey(Project)
     grant_type = models.ForeignKey(GrantType)
 
+    class Meta:
+        ordering = ['project', '-start_date']
+
     def __str__(self):
         return '%s: %s (%s-%s)' % (self.project.title, self.grant_type.grant_type,
                                    self.start_date.year, self.end_date.year if self.end_date else '')
