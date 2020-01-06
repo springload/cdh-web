72d4728154c32040cd712db76a0e32c4d0281f9d
Nick Budak <nbudak@princeton.edu>
2020-01-06 15:35:46 -0500

    install and configure wagtail 2.3

--- cdhweb/settings.py
+++ cdhweb/settings.py
@@ -108,6 +108,28 @@ RICHTEXT_ALLOWED_TAGS = ('a', 'abbr', 'acronym', 'address', 'area', 'article',
                         'textarea', 'tfoot', 'th', 'thead', 'tr', 'tt', '',
                         'ul', 'var', 'wbr')
 
+####################
+# WAGTAIL SETTINGS #
+####################
+
+# Human-readable name of your Wagtail site shown on login to the Wagtail admin.
+# https://docs.wagtail.io/en/latest/reference/settings.html#site-name
+WAGTAIL_SITE_NAME = 'CDH Website'
+
+# Wagtail sends email notifications when content is submitted for moderation,
+# and when the content is accepted or rejected.
+# https://docs.wagtail.io/en/latest/reference/settings.html#email-notifications
+WAGTAILADMIN_NOTIFICATION_FROM_EMAIL = 'admin@cdh.princeton.edu'
+WAGTAILADMIN_NOTIFICATION_USE_HTML = True
+
+# Tags are case-sensitive by default. In many cases the reverse is preferable.
+# https://docs.wagtail.io/en/latest/reference/settings.html#case-insensitive-tags
+TAGGIT_CASE_INSENSITIVE = True
+
+# Shows where a particular image, document or snippet is being used on your site.
+# Generates a query which may run slowly on sites with large numbers of pages.
+# https://docs.wagtail.io/en/latest/reference/settings.html#usage-for-images-documents-and-snippets
+WAGTAIL_USAGE_COUNT_ENABLED = True
 
 ########################
 # MAIN DJANGO SETTINGS #
@@ -305,7 +327,19 @@ INSTALLED_APPS = [
     # "mezzanine.twitter",
     # "mezzanine.accounts",
     # "mezzanine.mobile",
-    "taggit",
+    'wagtail.contrib.forms',
+    'wagtail.contrib.redirects',
+    'wagtail.embeds',
+    'wagtail.sites',
+    'wagtail.users',
+    'wagtail.snippets',
+    'wagtail.documents',
+    'wagtail.images',
+    'wagtail.search',
+    'wagtail.admin',
+    'wagtail.core',
+    'modelcluster',
+    'taggit',
     'adminsortable2',
     "compressor",
     "fullurl",
@@ -345,6 +379,8 @@ MIDDLEWARE = (
     # "mezzanine.core.middleware.SitePermissionMiddleware",
     "mezzanine.pages.middleware.PageMiddleware",
     "mezzanine.core.middleware.FetchFromCacheMiddleware",
+    'wagtail.core.middleware.SiteMiddleware',
+    'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 )
 
 # Store these package names here as they may change in the future since
