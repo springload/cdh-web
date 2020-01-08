ef0d31e08cc68b481456b2105b1df6202552070f
Nick Budak <nbudak@princeton.edu>
2020-01-08 14:35:48 -0500

    update migration tests

--- cdhweb/pages/tests/test_migrations.py
+++ cdhweb/pages/tests/test_migrations.py
@@ -1,45 +1,6 @@
 from django.apps import apps
-from django.test import TestCase
-from django.db.migrations.executor import MigrationExecutor
-from django.db import connection
 
-def get_parent(apps, page):
-    '''Find the parent of a wagtail page using its `path` attribute.'''
-    # see for an explanation of django-treebeard & the `path` attribute:
-    # http://www.agilosoftware.com/blog/django-treebard-and-wagtail-page-creation/
-    Page = apps.get_model('wagtailcore', 'Page')
-    return Page.objects.get(path=page.path[:4])
-
-
-# migration test case adapted from
-# https://www.caktusgroup.com/blog/2016/02/02/writing-unit-tests-django-migrations/
-# and from winthrop-django
-class TestMigrations(TestCase):
-
-    app = None
-    migrate_from = None
-    migrate_to = None
-
-    def setUp(self):
-        assert self.migrate_from and self.migrate_to, \
-            "TestCase '{}' must define migrate_from and migrate_to properties".format(type(self).__name__)
-        self.migrate_from = [(self.app, self.migrate_from)]
-        self.migrate_to = [(self.app, self.migrate_to)]
-        executor = MigrationExecutor(connection)
-        old_apps = executor.loader.project_state(self.migrate_from).apps
-
-        # Reverse to the original migration
-        executor.migrate(self.migrate_from)
-        self.setUpBeforeMigration(old_apps)
-
-        # Run the migration to test
-        executor.loader.build_graph()  # reload.
-        executor.migrate(self.migrate_to)
-
-        self.apps = executor.loader.project_state(self.migrate_to).apps
-
-    def setUpBeforeMigration(self, apps):
-        pass
+from cdhweb.pages.migration_utilities import TestMigrations, get_parent
 
 
 class TestCreateHomepage(TestMigrations):
