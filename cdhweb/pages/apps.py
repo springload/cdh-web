7d73ceb1559e60ce544a128895618789d3d5810b
Nick Budak <nbudak@princeton.edu>
2020-01-08 16:09:05 -0500

    update app label to fix NoReverseMatch issue

--- cdhweb/pages/apps.py
+++ cdhweb/pages/apps.py
@@ -3,4 +3,7 @@ from django.apps import AppConfig
 
 class PagesConfig(AppConfig):
     name = 'cdhweb.pages'
-    label = 'cdhweb.pages' # will conflict with mezzanine pages otherwise
+    # NOTE we have to relabel this to avoid a conflict with mezzanine's pages;
+    # using anything with a '.' in the name will confuse the RegexResolver and
+    # result in NoReverseMatch errors when adding pages.
+    label = 'cdhpages' 
