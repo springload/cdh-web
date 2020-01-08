34657c04c545f57dd0690435ab9abc17fa5afae3
Nick Budak <nbudak@princeton.edu>
2020-01-08 14:34:27 -0500

    unregister wagtail models from django admin

--- cdhweb/pages/admin.py
+++ cdhweb/pages/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from wagtail.core.models import Page, Site
+from wagtail.documents.models import Document
+from wagtail.images.models import Image
 
-# Register your models here.
+# unregister wagtail content from django admin to avoid
+# editing something in the wrong place and potentially causing
+# problems
+
+admin.site.unregister(Page)
+admin.site.unregister(Site)
+admin.site.unregister(Image)
+admin.site.unregister(Document)
