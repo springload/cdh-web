8e827c9a162699d38ef2a50457477697da80ea82
Nick Budak <nbudak@princeton.edu>
2020-09-03 16:40:27 -0400

    add virtual/url properties to admin

--- cdhweb/events/admin.py
+++ cdhweb/events/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import EventType, Location, Event
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('title', 'event_type', 'start_time', 'admin_thumb',
-        'attendance', 'tag_list')
+    list_display = ('title', 'event_type', 'is_virtual', 'start_time', 'admin_thumb',
+        'attendance', 'url', 'tag_list')
     prepopulated_fields = {"slug": ("title",)}
     date_hierarchy = 'start_time'
     search_fields = ('title', 'content')
@@ -16,12 +16,17 @@ class EventAdmin(admin.ModelAdmin):
         return u", ".join(o.name for o in obj.tags.all())
     tag_list.short_description = 'Tags'
 
+    def is_virtual(self, obj):
+        return "Yes" if obj.is_virtual else "No"
+    is_virtual.short_description = 'Virtual'
+
     # use inline fields for titles and resources
     # also: suppress management/auth fields like password, username, permissions,
     # last login and date joined
 
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'short_name', 'address')
+    list_display = ('name', 'short_name', 'address', 'is_virtual')
+    list_filter = ('is_virtual',)
 
 
 admin.site.register(EventType)
