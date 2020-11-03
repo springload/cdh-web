4dc1c7fa89f89628aefc0cf319dce6329c9a7855
Nick Budak <nbudak@princeton.edu>
2020-11-03 10:06:20 -0500

    Specify db port; remove STRICT_TRANS_TABLES

--- ci/testsettings.py
+++ ci/testsettings.py
@@ -12,10 +12,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': 'cdhweb',
         'HOST': '127.0.0.1',
-        'PORT': '',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'PORT': '3306',
     },
 }
 
