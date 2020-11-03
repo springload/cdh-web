6da8c51cb1807f7699b3522dc495730e5a106751
Nick Budak <nbudak@princeton.edu>
2020-11-03 09:25:13 -0500

    Update CI settings.py

--- ci/testsettings.py
+++ ci/testsettings.py
@@ -3,34 +3,24 @@ import os
 # This file is exec'd from settings.py, so it has access to and can
 # modify all the variables in settings.py.
 
-# If this file is changed in development, the development server will
-# have to be manually restarted because changes will not be noticed
-# immediately.
-
-# NOTE: setting debug = true to avoid pa11y-ci timeouts
-DEBUG = False
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cdhweb',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'cdhweb',
+        'PASSWORD': 'cdhweb',
         'HOST': '127.0.0.1',
-        'PORT': '',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'read_default_file': '~travis/.my.cnf'
         },
     },
 }
 
-# required with django 1.11 when debug is false, even for tests
-ALLOWED_HOSTS = ["*"]
+# turn off debug so we see 404s when testing
+DEBUG = False
 
 # configure django-compressor to compress css & javascript
-# NOTE: compression disabled because otherwise pa11y-ci times out
-COMPRESS_ENABLED = False
+COMPRESS_ENABLED = True
 
 # compress to the sitemedia folder
 COMPRESS_ROOT = os.path.join(BASE_DIR, 'sitemedia')
