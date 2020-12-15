453ecc4e223ec0fd2af60711f71a98520d07f74b
Kevin McElwee <kevinrmcelwee@gmail.com>
2020-12-15 13:42:17 -0500

    Shift django-dbml to test requirements and update settings

--- cdhweb/settings.py
+++ cdhweb/settings.py
@@ -323,7 +323,6 @@ INSTALLED_APPS = [
     "fullurl",
     "django_cas_ng",
     "pucas",
-    "django_dbml",
     # local apps
     "cdhweb.projects",
     "cdhweb.people",
@@ -398,6 +397,7 @@ OPTIONAL_APPS = (
     "debug_toolbar",
     "django_extensions",
     "compressor",
+    "django_dbml",
     PACKAGE_NAME_FILEBROWSER,
     PACKAGE_NAME_GRAPPELLI,
 )
