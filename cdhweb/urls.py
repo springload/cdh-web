179d76516b76453b09868656261bb7575ff5773e
Nick Budak <nbudak@princeton.edu>
2020-12-15 12:23:09 -0500

    Add djdt urls to main urlconf if DEBUG = True

--- cdhweb/urls.py
+++ cdhweb/urls.py
@@ -83,12 +83,13 @@ urlpatterns = [
 # https://docs.djangoproject.com/en/3.1/howto/static-files/#serving-files-uploaded-by-a-user-during-development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# django debug toolbar
-try:
-    import debug_toolbar
-    urlpatterns += path("__debug__/", include(debug_toolbar.urls)),
-except ImportError:
-    pass
+# add django debug toolbar urls if DEBUG = True
+if settings.DEBUG:
+    try:
+        import debug_toolbar
+        urlpatterns += path("__debug__/", include(debug_toolbar.urls)),
+    except ImportError:
+        pass
 
 # Adds ``STATIC_URL`` to the context of error pages, so that error
 # pages can use JS, CSS and images.
