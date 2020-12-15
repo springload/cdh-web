00ff73a1cda339825721a134f148ae034ff42ef4
Nick Budak <nbudak@princeton.edu>
2020-12-15 17:54:56 -0500

    Add homepage exodus

--- cdhweb/pages/management/commands/exodus.py
+++ cdhweb/pages/management/commands/exodus.py
@@ -7,6 +7,7 @@ from cdhweb.pages.models import HomePage, LandingPage, ContentPage
 from cdhweb.resources.models import LandingPage as OldLandingPage
 from mezzanine.pages.models import Page as MezzaninePage
 from wagtail.core.blocks import RichTextBlock
+from wagtail.core.models import Site, Page
 
 
 class Command(BaseCommand):
@@ -16,8 +17,30 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        homepage = HomePage.objects.get()
+        # TODO clear out all pages except root for idempotency
+        # Page.objects.filter(depth__gt=1).delete()
+        
+        root = Page.objects.get(depth=1)
 
+        # migrate the homepage
+        ohp = MezzaninePage.objects.get(slug="/")
+        homepage = HomePage(
+            title=ohp.title,
+            slug=ohp.slug,
+            seo_title=ohp._meta_title or ohp.title,
+            body=json.dumps([{
+                "type": "paragraph",
+                "value": ohp.richtextpage.content,
+            }]),
+            # Store description as search_description even if auto-generated
+            search_description=ohp.description,
+            first_published_at=ohp.created,
+            last_published_at=ohp.updated,
+        )
+        root.add_child(instance=homepage)
+        root.save()
+
+        # migrate all landing pages
         for olp in OldLandingPage.objects.all():
             lp = LandingPage(
                 title=olp.title,
@@ -28,24 +51,17 @@ class Command(BaseCommand):
                     "type": "paragraph",
                     "value": olp.content,
                 }]),
-                # Only store description if it's not auto-generated
                 search_description=olp.description,
                 first_published_at=olp.created,
                 last_published_at=olp.updated,
                 # TODO not dealing with images yet
-                # TODO not setting parent/child hierarchy yet
                 # TODO not setting menu placement yet
                 # TODO search keywords?
+                # TODO set the correct visibility status
                 # NOTE not login-restricting pages since we don't use it
+                # NOTE not setting expiry date; handled manually
+                # NOTE inclusion in sitemap being handled by sitemap itself
+                # NOTE set has_unpublished_changes on page?
             )
-            # TODO create a revision that sets the body content of the page
-            # TODO set the correct visibility status for the new revision
-            # TODO set the correct publication date for the new revision
-            # TODO set the created date for the new revision
-            # TODO set the last updated date for the new revision
-            # NOTE not setting expiry date; handled manually
-            # NOTE inclusion in sitemap being handled by sitemap itself
-            # NOTE set has_unpublished_changes on page?
             homepage.add_child(instance=lp)
-        
         homepage.save()
