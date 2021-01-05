82ce7a692684b8d5371431a9d8274684302a6908
Nick Budak <nbudak@princeton.edu>
2021-01-05 17:45:54 -0500

    Update People page tests

--- cdhweb/people/tests/test_pages.py
+++ cdhweb/people/tests/test_pages.py
@@ -1,5 +1,9 @@
-from cdhweb.people.models import PeopleLandingPage, ProfilePage, Person
-from cdhweb.pages.models import ContentPage, HomePage
+import json
+
+import pytest
+from cdhweb.pages.models import HomePage
+from cdhweb.people.models import PeopleLandingPage, Person, ProfilePage
+from django.core.exceptions import ValidationError
 from wagtail.core.models import Page
 from wagtail.tests.utils import WagtailPageTests
 from wagtail.tests.utils.form_data import (nested_form_data, rich_text,
@@ -11,25 +15,18 @@ class TestPeopleLandingPage(WagtailPageTests):
     def setUp(self):
         """create page tree for testing"""
         root = Page.objects.get(title="Root")
-        self.home = HomePage()
-        root.add_child(self.home)
+        self.home = HomePage(title="home", slug="")
+        root.add_child(instance=self.home)
         root.save()
 
-    def test_can_create(self):
-        """should be creatable as child of homepage"""
-        self.assertCanCreate(self.home, PeopleLandingPage, nested_form_data({
-            "title": "people",
-            "body": streamfield([("paragraph", rich_text("body text"))])
-        }))
-
     def test_parent_pages(self):
-        """only allowed parent is homepage"""
-        self.assertAllowedParentPageTypes(PeopleLandingPage, [HomePage])
+        """no allowed parent page type; must be created manually"""
+        self.assertAllowedParentPageTypes(PeopleLandingPage, [])
 
     def test_child_pages(self):
-        """content pages and profile pages can be children"""
+        """only profile pages can be children"""
         self.assertAllowedSubpageTypes(
-            PeopleLandingPage, [ContentPage, ProfilePage])
+            PeopleLandingPage, [ProfilePage])
 
 
 class TestProfilePage(WagtailPageTests):
@@ -37,23 +34,15 @@ class TestProfilePage(WagtailPageTests):
     def setUp(self):
         """create page tree and person for testing"""
         root = Page.objects.get(title="Root")
-        home = HomePage()
-        root.add_child(home)
+        home = HomePage(title="home", slug="")
+        root.add_child(instance=home)
         root.save()
-        self.lp = PeopleLandingPage()
-        home.add_child(self.lp)
+        self.lp = PeopleLandingPage(
+            title="people", slug="people", tagline="people of the cdh")
+        home.add_child(instance=self.lp)
         home.save()
         self.person = Person.objects.create(
             first_name="tom", last_name="jones")
-
-    def test_can_create(self):
-        """should be creatable as child of people landing page"""
-        self.assertCanCreate(self.lp, ProfilePage, nested_form_data({
-            "title": "tom r. jones",
-            "person": self.person,
-            "education": rich_text("school"),
-            "bio": streamfield([("paragraph", rich_text("about me"))])
-        }))
 
     def test_parent_pages(self):
         """only allowed parent is people landing page"""
@@ -63,9 +52,30 @@ class TestProfilePage(WagtailPageTests):
         """no allowed children"""
         self.assertAllowedSubpageTypes(ProfilePage, [])
 
+    def test_can_create(self):
+        """should be creatable as child of people landing page"""
+        profile = ProfilePage(
+            title="tom r. jones",
+            person=self.person,
+            education=rich_text("school"),
+            bio=json.dumps([{"type": "paragraph", "value": "about me"}])
+        )
+        self.lp.add_child(instance=profile)
+        self.lp.save()
+        assert self.person.profilepage == profile
+
+    def test_person_required(self):
+        """profile page must be for an existing person"""
+        with pytest.raises(ValidationError):
+            self.lp.add_child(instance=ProfilePage(
+                title="tom r. jones",
+                education=rich_text("school"),
+                bio=json.dumps([{"type": "paragraph", "value": "about me"}])
+            ))
+
     def test_delete_handler(self):
         """deleting person should delete corresponding profile"""
-        self.lp.add_child(ProfilePage(
+        self.lp.add_child(instance=ProfilePage(
             title="tom r. jones",
             person=self.person,
         ))
