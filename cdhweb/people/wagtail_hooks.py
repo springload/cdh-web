f0bf699b2e6540bf16084a6e40a20434d5b3596d
Nick Budak <nbudak@princeton.edu>
2021-01-05 15:11:27 -0500

    Update admin for People and ProfilePages

--- cdhweb/people/wagtail_hooks.py
+++ cdhweb/people/wagtail_hooks.py
@@ -7,10 +7,13 @@ from wagtail.contrib.modeladmin.options import (ModelAdmin, ModelAdminGroup,
 class PersonAdmin(ThumbnailMixin, ModelAdmin):
     model = Person
     menu_icon = "group"
-    list_display = ("admin_thumb", "first_name", "last_name", "current_title",
+    list_display = ("first_name", "last_name", "admin_thumb", "current_title",
                     "cdh_staff")
     search_fields = ("first_name", "last_name", "user__username")
     list_filter = ("pu_status", "cdh_staff")
+    list_export = ("first_name", "last_name", "admin_thumb", "current_title",
+                   "cdh_staff")
+    export_filename = "cdhweb-people"
     thumb_image_field_name = "image"
 
 
@@ -20,13 +23,17 @@ class TitleAdmin(ModelAdmin):
     list_display = ("title", "sort_order", "num_people")
     search_fields = ("title",)
 
+# remove ProfilePages from the regular wagtail page editor and create a special
+# admin area for them underneath the "people" section
+
 
 class ProfilePageAdmin(ThumbnailMixin, ModelAdmin):
     model = ProfilePage
     menu_icon = "user"
-    list_display = ("admin_thumb", "title")
+    list_display = ("title", "admin_thumb", "live")
     search_fields = ("title",)
     thumb_image_field_name = "image"
+    exclude_from_explorer = True
 
 
 class PeopleGroup(ModelAdminGroup):
@@ -37,5 +44,4 @@ class PeopleGroup(ModelAdminGroup):
 
 
 # TODO inlines for editing PersonRelatedLinks (#181) on People
-# TODO ProfilePage admin
 modeladmin_register(PeopleGroup)
