d7516e3fd206fda7825f2ae0d057b4a63ff00104
Nick Budak <nbudak@princeton.edu>
2021-01-15 11:18:25 -0500

    Set up Wagtail admin for Projects app

---
+++ cdhweb/projects/wagtail_hooks.py
@@ -0,0 +1,43 @@
+from cdhweb.projects.models import ProjectPage, GrantType, Role
+from wagtail.contrib.modeladmin.mixins import ThumbnailMixin
+from wagtail.contrib.modeladmin.options import (ModelAdmin, ModelAdminGroup,
+                                                modeladmin_register)
+
+
+class ProjectAdmin(ThumbnailMixin, ModelAdmin):
+    model = ProjectPage
+    menu_label = "Projects"
+    menu_icon = "site"
+    list_display = ("title", "live", "highlight", "admin_thumb", "tags")
+    list_editable = ("live", "highlight")
+    list_filter = ("live", "cdh_built", "working_group")
+    list_export = ("title", "working_group", "cdh_built", "tags",
+                   "short_description", "long_description", "website_url",
+                   "updated_at")
+    search_fields = ("title", "short_description", "grant__grant_type")
+
+
+class GrantTypeAdmin(ModelAdmin):
+    model = GrantType
+    menu_icon = "pick"
+    list_display = ("grant_type",)
+    search_fields = ("grant_type",)
+
+
+class RoleAdmin(ModelAdmin):
+    model = Role
+    menu_icon = "group"
+    list_display = ("title", "sort_order")
+    list_editable = ("title", "sort_order")
+    search_fields = ("title",)
+
+
+class ProjectsGroup(ModelAdminGroup):
+    menu_label = "Projects"
+    menu_icon = "site"
+    menu_order = 210
+    items = (ProjectAdmin, RoleAdmin, GrantTypeAdmin)
+
+
+# TODO inlines for editing ProjectRelatedLinks (#181) on Project
+modeladmin_register(ProjectsGroup)
