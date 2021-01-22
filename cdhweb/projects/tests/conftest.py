cff7f53934918faa82e97442c5840a9ba24612cc
Nick Budak <nbudak@princeton.edu>
2021-01-22 10:39:31 -0500

    Add postdoc project fixture, update end dates

--- cdhweb/projects/tests/conftest.py
+++ cdhweb/projects/tests/conftest.py
@@ -34,8 +34,8 @@ def derrida(db, projects_landing_page):
     projects_landing_page.save()
 
     # dates used
-    today = datetime.today()
-    one_year_ago = today - timedelta(days=365)
+    tomorrow = datetime.today() + timedelta(days=1)
+    one_year_ago = datetime.today() - timedelta(days=365)
     two_years_ago = one_year_ago - timedelta(days=365)
 
     # add grants: DCG from last year and RPG from this year
@@ -44,21 +44,21 @@ def derrida(db, projects_landing_page):
     Grant.objects.create(project=derrida, grant_type=dcg,
                          start_date=two_years_ago, end_date=one_year_ago)
     Grant.objects.create(project=derrida, grant_type=rpg,
-                         start_date=one_year_ago, end_date=today)
+                         start_date=one_year_ago, end_date=tomorrow)
 
     # add memberships for initial project team (first year)
     add_project_member(derrida, "Project Director", start_date=two_years_ago,
-                       end_date=today, first_name="Katie", last_name="Chenoweth")
+                       end_date=tomorrow, first_name="Katie", last_name="Chenoweth")
     add_project_member(derrida, "Lead Developer", start_date=two_years_ago,
-                       end_date=today, first_name="Rebecca", last_name="Koeser")
+                       end_date=tomorrow, first_name="Rebecca", last_name="Koeser")
     add_project_member(derrida, "Project Manager", start_date=two_years_ago,
                        end_date=one_year_ago, first_name="Rebecca", last_name="Munson")
 
     # add new memberships for past year
     add_project_member(derrida, "Grad Assistant", start_date=one_year_ago,
-                       end_date=today, first_name="Chloe", last_name="Vettier")
+                       end_date=tomorrow, first_name="Chloe", last_name="Vettier")
     add_project_member(derrida, "Project Manager", start_date=one_year_ago,
-                       end_date=today, first_name="Renee", last_name="Altergott")
+                       end_date=tomorrow, first_name="Renee", last_name="Altergott")
 
     return derrida
 
@@ -72,14 +72,34 @@ def pliny(db, projects_landing_page):
     projects_landing_page.save()
 
     # add a staff r&d grant and one member; no end date so grant/role is current
-    one_year_ago = datetime.today() - timedelta(days=365)
+    start_date = datetime.today() - timedelta(days=400)
     srd = GrantType.objects.get_or_create(grant_type="Staff R&D")[0]
     Grant.objects.create(project=pliny, grant_type=srd,
-                         start_date=one_year_ago)
+                         start_date=start_date)
     add_project_member(pliny, "Project Director",
-                       start_date=one_year_ago, first_name="Ben", last_name="Hicks")
+                       start_date=start_date, first_name="Ben", last_name="Hicks")
 
     return pliny
+
+
+@pytest.fixture
+def ocampo(db, projects_landing_page):
+    """a postdoctoral research project with one r&d grant and one current member"""
+    # create the project
+    ocampo = Project(title="Global Networks of Cultural Production")
+    projects_landing_page.add_child(instance=ocampo)
+    projects_landing_page.save()
+
+    # add a postdoc grant and one member; no end date so grant/role is current
+    start_date = datetime.today() - timedelta(days=450)
+    prp = GrantType.objects.get_or_create(
+        grant_type="Postdoctoral Research Project")[0]
+    Grant.objects.create(project=ocampo, grant_type=prp,
+                         start_date=start_date)
+    add_project_member(ocampo, "Project Director",
+                       start_date=start_date, first_name="Nora", last_name="Benedict")
+
+    return ocampo
 
 
 @pytest.fixture
@@ -91,8 +111,7 @@ def slavic(db, projects_landing_page):
     projects_landing_page.save()
 
     # dates used
-    today = datetime.today()
-    one_year_ago = today - timedelta(days=365)
+    one_year_ago = datetime.today() - timedelta(days=365)
     two_years_ago = one_year_ago - timedelta(days=365)
 
     # add a seed grant and one member; grant ended but membership is current
