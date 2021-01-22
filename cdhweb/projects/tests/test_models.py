e4c55da18b2cc1cae7c4d77037b8bc47f195d94a
Nick Budak <nbudak@princeton.edu>
2021-01-22 11:01:57 -0500

    Update tests for Membership, Grant, ProjectRelatedLink

--- cdhweb/projects/tests/test_models.py
+++ cdhweb/projects/tests/test_models.py
@@ -1,14 +1,7 @@
-import pytest
-from datetime import date, datetime, timedelta
+from datetime import datetime, timedelta
 
 from cdhweb.pages.models import RelatedLinkType
-from cdhweb.projects.models import (Grant, GrantType, Membership, Project,
-                                    ProjectRelatedLink, Role)
-from django.contrib.auth import get_user_model
-from django.test import TestCase
-from django.utils import timezone
-from mezzanine.core.models import (CONTENT_STATUS_DRAFT,
-                                   CONTENT_STATUS_PUBLISHED)
+from cdhweb.projects.models import GrantType, Project, ProjectRelatedLink, Role
 
 
 class TestGrantType:
@@ -25,7 +18,6 @@ class TestRole:
         assert str(role) == role.title
 
 
-@pytest.mark.django_db
 class TestProjectQuerySet:
 
     def test_highlighted(self, derrida):
@@ -112,51 +104,45 @@ class TestProjectQuerySet:
         assert ordered[3] == slavic     # seed grant 2yrs ago
 
 
-@pytest.mark.skip
-class TestGrant(TestCase):
+class TestGrant:
 
-    def test_str(self):
-        proj = Project.objects.create(title="Derrida's Margins")
-        grtype = GrantType.objects.create(grant_type='Sponsored Project')
-        start_year, end_year = (2016, 2017)
-        grant = Grant(project=proj, grant_type=grtype,
-                      start_date=datetime(start_year, 1, 1),
-                      end_date=datetime(end_year, 1, 1))
-        assert str(grant) == '%s: %s (2016–2017)' % \
-            (proj.title, grtype.grant_type)
+    def test_str(self, derrida, pliny):
+        """should display grant type, project title, and start/end dates"""
+        # check derrida's margins RPG with both dates
+        rpg = derrida.latest_grant()
+        assert str(rpg) == "Derrida's Margins: Research Partnership (%s–%s)" % (
+            rpg.start_date.year, rpg.end_date.year)
 
-
-@pytest.mark.skip
-class TestMembership(TestCase):
-
-    def test_str(self):
-        # create test objects needed for a membership
-        proj = Project.objects.create(title="Derrida's Margins")
-        grtype = GrantType.objects.create(grant_type='Sponsored Project')
-        grant = Grant.objects.create(project=proj, grant_type=grtype,
-                                     start_date=datetime(2016, 1, 1),
-                                     end_date=datetime(2017, 1, 1))
-        user = get_user_model().objects.create(username='contributor')
-        role = Role.objects.create(title='Data consultant', sort_order=1)
-        membership = Membership.objects.create(
-            project=proj, person=user, role=role, start_date=grant.start_date)
-
-        assert str(membership) == '%s - %s on %s (%s)' % (user, role, proj,
-                                                          membership.years)
+        # check pliny r&d grant with no end date
+        srg = pliny.latest_grant()
+        assert str(srg) == "Pliny Project: Staff R&D (%s–)" % srg.start_date.year
 
 
-@pytest.mark.skip
-class TestProjectRelatedLink(TestCase):
+class TestMembership:
 
-    def test_display_url(self):
-        base_url = 'derridas-margins.princeton.edu'
-        project_url = 'http://%s' % base_url
-        proj = Project.objects.create(title="Derrida's Margins")
-        website = RelatedLinkType.objects.get(name='Website')
-        res = ProjectRelatedLink.objects.create(project=proj, type=website,
+    def test_str(self, derrida, pliny):
+        # check derrida's margins RPG membership with both dates
+        katie = derrida.memberships.get(person__first_name="Katie")
+        assert str(
+            katie) == "Katie Chenoweth - Project Director on Derrida's Margins (%s)" % katie.years
+        # check pliny membership with no end date
+        ben = pliny.memberships.first()
+        assert str(
+            ben) == "Ben Hicks - Project Director on Pliny Project (%s)" % ben.years
+
+
+class TestProjectRelatedLink:
+
+    def test_display_url(self, derrida):
+        """should remove http/https from website url for display"""
+        # http
+        base_url = "derridas-margins.princeton.edu"
+        project_url = "http://%s" % base_url
+        website = RelatedLinkType.objects.get_or_create(name="Website")[0]
+        res = ProjectRelatedLink.objects.create(project=derrida, type=website,
                                                 url=project_url)
         assert res.display_url() == base_url
 
         # https
-        res.url = 'https://%s' % base_url
+        res.url = "https://%s" % base_url
         assert res.display_url() == base_url
