25238db414aebbc23008a2b52544132410dc424d
Nick Budak <nbudak@princeton.edu>
2021-01-22 11:59:43 -0500

    Update projects urls

--- cdhweb/projects/urls.py
+++ cdhweb/projects/urls.py
@@ -4,8 +4,7 @@ from cdhweb.projects import views
 
 app_name = 'projects'
 urlpatterns = [
-    url('^$', views.ProjectListView.as_view(), name='list'),
+    url('^$', views.SponsoredProjectListView.as_view(), name='list'),
     url('^staff/$', views.StaffProjectListView.as_view(), name='staff'),
     url('^working-groups/$', views.WorkingGroupListView.as_view(), name='working-groups'),
-    url(r'^(?P<slug>[\w-]+)/$', views.ProjectDetailView.as_view(), name='detail'),
 ]
