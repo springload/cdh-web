b0a5eb17bface3e4b20897db34dd434cd802932f
Nick Budak <nbudak@princeton.edu>
2021-02-18 16:11:52 -0500

    Update events urls; remove event detail url

--- cdhweb/events/urls.py
+++ cdhweb/events/urls.py
@@ -2,15 +2,13 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'events'
+app_name = "events"
 urlpatterns = [
-    url('^$', views.UpcomingEventsView.as_view(), name='upcoming'),
-    url('^(?P<pk>\d{1,2})/$', views.EventRedirectView.as_view(),
-        name='by-id'),
-    url('^(?P<semester>(fall|spring|summer))-(?P<year>\d{4})/$',
-        views.EventSemesterArchiveView.as_view(), name='by-semester'),
-    url("^(?P<year>\d{4})/(?P<month>\d{2})/(?P<slug>[\w-]+)/$",
-        views.EventDetailView.as_view(), name='detail'),
+    url("^$", views.UpcomingEventsView.as_view(), name="upcoming"),
+    url("^(?P<pk>\d{1,2})/$", views.EventRedirectView.as_view(),
+        name="by-id"),
+    url("^(?P<semester>(fall|spring|summer))-(?P<year>\d{4})/$",
+        views.EventSemesterArchiveView.as_view(), name="by-semester"),
     url("^(?P<year>\d{4})/(?P<month>\d{2})/(?P<slug>[\w-]+).ics$",
-        views.EventIcalView.as_view(), name='ical'),
+        views.EventIcalView.as_view(), name="ical"),
 ]
