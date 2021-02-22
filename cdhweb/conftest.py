b452bea98d944607c37a56ff34b5b262b50f4ef6
Nick Budak <nbudak@princeton.edu>
2021-02-22 13:33:05 -0500

    Fallback strategy for importing large fixtures
    We need to import some fixtures and share among many modules,
    and these fixtures in turn export others specific to that module.
    This uses a wildcard import to solve the problem, since there's
    no guidance from pytest about how to accomplish it otherwise.

--- cdhweb/conftest.py
+++ cdhweb/conftest.py
@@ -1,3 +1,8 @@
 """Fixtures/utilities that should be globally available for testing."""
 from cdhweb.pages.tests.conftest import site, homepage
-from cdhweb.projects.tests.conftest import projects_link_page
+# FIXME not sure how else to share fixtures that depend on other fixtures
+# between modules - if you import just the top-level fixture (e.g. "events"),
+# it fails to find the fixture dependencies, and so on all the way down. For
+# now this does what we want, although it pollutes the namespace somewhat
+from cdhweb.events.tests.conftest import *
+from cdhweb.projects.tests.conftest import *
