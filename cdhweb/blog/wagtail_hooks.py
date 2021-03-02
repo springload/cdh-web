b3a25aa594fb8314b09420b5719026c3b3080bc6
Nick Budak <nbudak@princeton.edu>
2021-03-02 16:18:41 -0500

    Add authors to blogpost admin

--- cdhweb/blog/wagtail_hooks.py
+++ cdhweb/blog/wagtail_hooks.py
@@ -6,14 +6,14 @@ from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 class BlogPostAdmin(ThumbnailMixin, ModelAdmin):
     model = BlogPost
     menu_icon = "edit"
-    list_display = ("admin_thumb", "title",
+    list_display = ("admin_thumb", "title", "author_list",
                     "first_published_at", "live", "is_featured")
     list_display_add_buttons = "title"
     list_filter = ("is_featured", "first_published_at")
-    list_export = ("title", "first_published_at", "is_featured",
+    list_export = ("title", "author_list", "first_published_at", "is_featured",
                    "featured_image", "tags", "content")
     export_filename = "cdhweb-blogposts"
-    search_fields = ("title", "content")
+    search_fields = ("title", "author_list", "content")
     exclude_from_explorer = True
     thumb_image_field_name = "featured_image"
     thumb_col_header_text = "image"
