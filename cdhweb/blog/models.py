5c2b62e6ac57ae08ad51f4f19cf7a706de201891
Nick Budak <nbudak@princeton.edu>
2021-03-02 15:05:56 -0500

    Add/update Blog models (#185)

--- cdhweb/blog/models.py
+++ cdhweb/blog/models.py
@@ -1,17 +1,24 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
-from django.utils.translation import ugettext_lazy as _
 from django.utils.text import Truncator
-
+from django.utils.translation import ugettext_lazy as _
 from mezzanine.core.fields import FileField
 from mezzanine.core.models import Displayable, RichText
 from mezzanine.utils.models import AdminThumbMixin, upload_to
-
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
 from taggit.managers import TaggableManager
+from taggit.models import TaggedItemBase
+from wagtail.admin.edit_handlers import (FieldPanel, FieldRowPanel,
+                                         PageChooserPanel, StreamFieldPanel)
+from wagtail.core.fields import StreamField
+from wagtail.core.models import Page, PageManager, PageQuerySet
+from wagtail.images.edit_handlers import ImageChooserPanel
 
-# from cdhweb.people.models import Person
-from cdhweb.resources.models import Attachment, PublishedQuerySetMixin
+from cdhweb.pages.models import BodyContentBlock, LinkPage
+from cdhweb.resources.models import Attachment
 
 
 class MultiOwnable(models.Model):
@@ -22,7 +29,7 @@ class MultiOwnable(models.Model):
     """
 
     users = models.ManyToManyField(User, verbose_name=_("Authors"),
-        related_name="%(class)ss")
+                                   related_name="%(class)ss")
 
     class Meta:
         abstract = True
@@ -40,15 +47,15 @@ class MultiOwnable(models.Model):
     author_list.short_description = 'Authors'
 
 
-class BlogPostQuerySet(PublishedQuerySetMixin):
+class BlogPostQuerySet(PageQuerySet):
 
     def featured(self):
         '''return blog posts that are marked as featured'''
         return self.filter(is_featured=True)
 
     def recent(self):
-        '''sort blog posts by publication date'''
-        return self.order_by('-publish_date')
+        '''sort blog posts by first publication date'''
+        return self.order_by('-first_published_at')
 
 
 class BlogPost(Displayable, MultiOwnable, RichText, AdminThumbMixin):
@@ -60,15 +67,16 @@ class BlogPost(Displayable, MultiOwnable, RichText, AdminThumbMixin):
     # mezzanine blogpost has comments, categoriees, and ratings; do we
     # care about any of those?
     featured_image = FileField(verbose_name=_("Featured Image"),
-        upload_to=upload_to("blog.BlogPost.featured_image", "blog"),
-        help_text="Appears on the homepage carousel when post is featured.",
-        format="Image", max_length=255, null=True, blank=True)
+                               upload_to=upload_to(
+                                   "blog.BlogPost.featured_image", "blog"),
+                               help_text="Appears on the homepage carousel when post is featured.",
+                               format="Image", max_length=255, null=True, blank=True)
     related_posts = models.ManyToManyField("self",
-                                 verbose_name=_("Related posts"), blank=True)
+                                           verbose_name=_("Related posts"), blank=True)
     tags = TaggableManager(blank=True)
     attachments = models.ManyToManyField(Attachment, blank=True)
     is_featured = models.BooleanField(verbose_name=_("Featured"), default=False,
-                help_text="Feature the post in the carousel on the homepage.")
+                                      help_text="Feature the post in the carousel on the homepage.")
 
     admin_thumb_field = "featured_image"
 
@@ -100,3 +108,90 @@ class BlogPost(Displayable, MultiOwnable, RichText, AdminThumbMixin):
             'month': '%02d' % self.publish_date.month,
             'day': '%02d' % self.publish_date.day,
             'slug': self.slug})
+
+
+# custom manager for wagtail pages, see:
+# https://docs.wagtail.io/en/stable/topics/pages.html#custom-page-managers
+BlogPostManager = PageManager.from_queryset(BlogPostQuerySet)
+
+
+class BlogPostTag(TaggedItemBase):
+    """Tags for Blog posts."""
+    content_object = ParentalKey(
+        "blog.BlogPostPage", on_delete=models.CASCADE, related_name="tagged_items")
+
+
+class BlogPostPage(Page, ClusterableModel):
+    """A Blog post, implemented as a Wagtail page."""
+
+    content = StreamField(BodyContentBlock, blank=True)
+    featured_image = models.ForeignKey("wagtailimages.image", null=True, blank=True,
+                                       on_delete=models.SET_NULL, related_name="+",
+                                       help_text="Appears on the homepage carousel when post is featured.")
+    related_posts = models.ManyToManyField("self", blank=True)
+    tags = ClusterTaggableManager(through=BlogPostTag, blank=True)
+    updated = models.DateTimeField(auto_now=True, null=True, editable=False)
+    is_featured = models.BooleanField(
+        default=False, help_text="Show the post in the carousel on the homepage.")
+    # TODO attachments (#245)
+
+    # can only be created underneath special link page
+    parent_page_types = ["blog.BlogLinkPage"]
+    # no allowed subpages
+    subpage_types = []
+
+    # admin edit configuration
+    content_panels = Page.content_panels + [
+        FieldRowPanel((ImageChooserPanel("featured_image"),
+                       FieldPanel("is_featured"))),
+        PageChooserPanel("related_posts"),
+        StreamFieldPanel("content"),
+    ]
+    promote_panels = Page.promote_panels + [
+        FieldPanel("tags")
+    ]
+
+    # custom manager/queryset logic
+    objects = BlogPostManager()
+
+    context_object_name = "post"
+
+    class Meta:
+        ordering = ("-first_published_at",)
+
+    @property
+    def short_title(self):
+        """Shorter title with ellipsis."""
+        return Truncator(self.title).chars(65)
+
+    @property
+    def short_description(self):
+        """Shorter description with ellipsis."""
+        return Truncator(self.seo_description).chars(250)
+
+    def get_url_parts(self, *args, **kwargs):
+        """Custom blog post URLs of the form /updates/2014/03/01/my-post."""
+        url_parts = super().get_url_parts(*args, **kwargs)
+        # NOTE evidently this can sometimes be None; unclear why – perhaps it
+        # gets called in a context where the request is unavailable? Only
+        # happens in QA, not locally.
+        if url_parts:
+            site_id, root_url, _ = url_parts
+            page_path = reverse("blog:detail", kwargs={
+                "year": self.first_published_at.year,
+                # force two-digit month and day
+                "month": "%02d" % self.first_published_at.month,
+                "day": "%02d" % self.first_published_at.day,
+                "slug": self.slug,
+            })
+            return site_id, root_url, page_path
+
+
+class BlogLinkPage(LinkPage):
+    """Container page that defines where blog posts can be created."""
+    # NOTE this page can't be created in the page editor; it is only ever made
+    # via a script or the console, since there's only one.
+    parent_page_types = []
+    # NOTE the only allowed child page type is a BlogPost; this is so that
+    # Events made in the admin automatically are created here.
+    subpage_types = [BlogPostPage]
