120198af299ab605ee5e355fddd692c19881f5e4
Nick Budak <nbudak@princeton.edu>
2021-03-08 15:46:33 -0500

    Add more blogpost fixtures

--- cdhweb/blog/tests/conftest.py
+++ cdhweb/blog/tests/conftest.py
@@ -23,7 +23,7 @@ def announcement(db, blog_link_page):
     yesterday = timezone.now() - timedelta(days=1)
     post = BlogPost(
         title="A Big Announcement!",
-        content=json.dumps([{
+        body=json.dumps([{
             "type": "paragraph",
             "value": "<p>here's the text of the announcement</p>"
         }])
@@ -38,12 +38,12 @@ def announcement(db, blog_link_page):
 
 
 @pytest.fixture
-def project_feature(db, blog_link_page, staffer):
+def project_feature(db, blog_link_page, grad_pm):
     """a post about a sponsored project published 4 weeks ago with one author"""
     one_month_ago = timezone.now() - timedelta(weeks=4)
     post = BlogPost(
         title="Making progress on the Cool Project",
-        content=json.dumps([{
+        body=json.dumps([{
             "type": "paragraph",
             "value": "<p>the Cool Project is getting cooler every day</p>"
         }])
@@ -52,14 +52,35 @@ def project_feature(db, blog_link_page, staffer):
     blog_link_page.save()
     post.first_published_at = one_month_ago
     post.save()
-    Author.objects.create(person=staffer, post=post)
+    Author.objects.create(person=grad_pm, post=post)
     return post
 
 
 @pytest.fixture
-def blog_posts(db, announcement, project_feature):
+def article(db, blog_link_page, staffer, postdoc):
+    """an article co-written by a staff member and a postdoc from a year ago"""
+    one_year_ago = timezone.now() - timedelta(weeks=52)
+    post = BlogPost(
+        title="We wrote an article together, and it got published on the CDH website",
+        body=json.dumps([{
+            "type": "paragraph",
+            "value": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }])
+    )
+    blog_link_page.add_child(instance=post)
+    blog_link_page.save()
+    post.first_published_at = one_year_ago
+    post.save()
+    Author.objects.create(person=staffer, post=post)
+    Author.objects.create(person=postdoc, post=post)
+    return post
+
+
+@pytest.fixture
+def blog_posts(db, announcement, project_feature, article):
     """convenience fixture to create several blog posts for testing"""
     return {
         "announcement": announcement,
-        "project_feature": project_feature
+        "project_feature": project_feature,
+        "article": article
     }
