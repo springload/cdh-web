d103fcebdb7e6bbb385370f3c68d65a8fae6cdc8
Nick Budak <nbudak@princeton.edu>
2021-03-16 14:06:27 -0400

    Add basic tests for blog RSS view

--- cdhweb/blog/tests/test_views.py
+++ cdhweb/blog/tests/test_views.py
@@ -1,5 +1,8 @@
-from datetime import date
+from cdhweb.blog.tests.conftest import announcement
+from datetime import date, timezone
 from django.urls import reverse
+
+from pytest_django.asserts import assertContains
 
 
 class TestBlogYearArchiveView:
@@ -74,3 +77,47 @@ class TestBlogPostDetailView:
         response = client.get(announcement.get_url())
         assert response.context["previous"] == feature
         assert response.context["next"] == None
+
+
+class TestRssBlogPostFeed:
+
+    def test_titles(self, client, blog_posts):
+        """rss feed should list post titles"""
+        response = client.get(reverse("blog:rss"))
+        for _, post in blog_posts.items():
+            assertContains(response, "<title>%s</title>" % post.title)
+
+    def test_descriptions(self, client, blog_posts):
+        """rss feed should list post descriptions"""
+        response = client.get(reverse("blog:rss"))
+        for _, post in blog_posts.items():
+            assertContains(response, "<description>%s</description>" %
+                           post.get_description())
+
+    def test_links(self, client, blog_posts):
+        """rss feed should list post descriptions"""
+        response = client.get(reverse("blog:rss"))
+        for _, post in blog_posts.items():
+            assertContains(response, "<link>%s</link>" % post.get_full_url())
+
+    def test_author_names(self, client, blog_posts):
+        """rss feed should list author names and emails if present"""
+        response = client.get(reverse("blog:rss"))
+        # author name is combined with email, if present and single author
+        assertContains(response, "<author>tom@princeton.edu (Tom)</author>")
+
+    def test_pubdates(self, client, blog_posts):
+        """rss feed should list post publication dates"""
+        response = client.get(reverse("blog:rss"))
+        # all dates will display as their UTC versions
+        for _, post in blog_posts.items():
+            assertContains(response, "<pubDate>%s</pubDate>" %
+                post.first_published_at.astimezone(timezone.utc).strftime("%a, %d %b %Y %H:%M:%S %z"))
+
+    def test_last_modified(self, client, blog_posts):
+        """rss feed should list most recent modification date for whole feed"""
+        response = client.get(reverse("blog:rss"))
+        # most recently modified post was announcement; displays as UTC
+        announcement = blog_posts["announcement"]
+        assertContains(response, "<lastBuildDate>%s</lastBuildDate>" %
+            announcement.last_published_at.astimezone(timezone.utc).strftime("%a, %d %b %Y %H:%M:%S %z"))
