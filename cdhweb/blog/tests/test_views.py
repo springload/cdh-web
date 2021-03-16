1a1a3906347a91570ee85d7352d6195238da6dc2
Nick Budak <nbudak@princeton.edu>
2021-03-16 11:39:01 -0400

    Add tests for blog year/month archive views

--- cdhweb/blog/tests/test_views.py
+++ cdhweb/blog/tests/test_views.py
@@ -1,3 +1,49 @@
+from datetime import date
+from django.urls import reverse
+
+
+class TestBlogYearArchiveView:
+
+    def test_month_list(self, client, blog_posts):
+        """blog post year archive should add months for all posts to context"""
+        # year is irrelevant; we always want all months to render the dropdown
+        response = client.get(reverse("blog:by-year", kwargs={"year": "2019"}))
+        dates = [post.first_published_at for _title,
+                 post in blog_posts.items()]
+        # months will appear as datetime.date objects with day set to 1
+        months = [date(pubdate.year, pubdate.month, 1) for pubdate in dates]
+        # should be ordered with most recent first
+        assert list(response.context["date_list"]) == \
+            sorted(months, reverse=True)
+
+    def test_title(self, client, blog_posts):
+        """blog post year archive should add year to context as title"""
+        response = client.get(reverse("blog:by-year", kwargs={"year": "2019"}))
+        assert response.context["title"] == "2019"
+
+
+class TestBlogMonthArchiveView:
+
+    def test_month_list(self, client, blog_posts):
+        """blog post year archive should add months for all posts to context"""
+        # args are irrelevant; we always want all months to render the dropdown
+        response = client.get(reverse("blog:by-month",
+                              kwargs={"year": "2019", "month": "03"}))
+        dates = [post.first_published_at for _title,
+                 post in blog_posts.items()]
+        # months will appear as datetime.date objects with day set to 1
+        months = [date(pubdate.year, pubdate.month, 1) for pubdate in dates]
+        # should be ordered with most recent first
+        assert list(response.context["date_list"]) == \
+            sorted(months, reverse=True)
+
+    def test_title(self, client, blog_posts):
+        """blog post year archive should add year/month to context as title"""
+        response = client.get(reverse("blog:by-month",
+                              kwargs={"year": "2019", "month": "03"}))
+        assert response.context["title"] == "March 2019"
+
+
 class TestBlogPostDetailView:
 
     def test_context_obj(self, client, announcement):
