73ffe04ca125be5420e9c555055f4827ec68e4fc
rlskoeser <rebecca.s.koeser@princeton.edu>
2021-04-01 11:55:54 -0400

    Move error templates to standard location; drop mezzanine error handlers

--- cdhweb/people/views.py
+++ cdhweb/people/views.py
@@ -239,7 +239,8 @@ class ExecListView(PersonListView):
 def speakerlist_gone(request):
     # return 410 gone for speakers list view;
     # (removed in 3.0, no longer needed after the Year of Data)
-    return render(request, 'errors/404.html', context={
+    return render(request, '404.html', context={
         'error_code': 410,
-        'message': "That page isn't here anymore."
+        'message': "That page isn't here anymore.",
+        'page_title': 'Erorr — no longer available'
     }, status=410)
