467aa050d82f952e502146abfbe97bca562b18ab
rlskoeser <rebecca.s.koeser@princeton.edu>
2021-05-04 11:18:49 -0400

    Configure file-system caching

--- cdhweb/settings.py
+++ cdhweb/settings.py
@@ -303,6 +303,13 @@ PUCAS_LDAP = {
 }
 
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/var/tmp/django_cache',
+    }
+}
+
 #########################
 # OPTIONAL APPLICATIONS #
 #########################
