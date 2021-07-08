137aeef775adea234b646fc647fdddf58b9d7476
Nick Budak <nbudak@princeton.edu>
2021-07-08 12:31:12 -0400

    Add implicit wait so fonts load; snapshot 404 page

--- ci/visual_tests.py
+++ ci/visual_tests.py
@@ -27,53 +27,67 @@ def run():
 
     # homepage
     browser.get("http://localhost:8000/")
+    browser.implicitly_wait(10)
     percy_snapshot(browser, "Home")
 
     # landing page
     browser.get("http://localhost:8000/landing")
+    browser.implicitly_wait(10)
     percy_snapshot(browser, "Landing Page")
 
     # content page
-    browser.get("http://localhost:8000/content")
+    browser.get("http://localhost:8000/landing/content")
+    browser.implicitly_wait(10)
     percy_snapshot(browser, "Content Page")
 
     # projects list page
     browser.get("http://localhost:8000/projects/sponsored")
+    browser.implicitly_wait(10)
     browser.find_element_by_class_name("toggle").click()  # expand dropdown
     percy_snapshot(browser, "Projects List Page")
 
     # project page
     browser.get("http://localhost:8000/projects/derridas-margins/")
+    browser.implicitly_wait(10)
     percy_snapshot(browser, "Project Page")
 
     # people list page
     browser.get("http://localhost:8000/people/staff")
+    browser.implicitly_wait(10)
     browser.find_element_by_class_name("toggle").click()
     percy_snapshot(browser, "Person List Page")
 
     # profile page
     browser.get("http://localhost:8000/people/staffer/")
+    browser.implicitly_wait(10)
     percy_snapshot(browser, "Profile Page")
 
     # events list page
     browser.get("http://localhost:8000/events")
+    browser.implicitly_wait(10)
     browser.find_element_by_class_name("toggle").click()
     percy_snapshot(browser, "Events List Page")
 
     # event page
     browser.get("http://localhost:8000/events/2017/02/testing-course/")
+    browser.implicitly_wait(10)
     percy_snapshot(browser, "Event Page")
 
     # blog list page
     browser.get("http://localhost:8000/updates")
+    browser.implicitly_wait(10)
     browser.find_element_by_class_name("toggle").click()
     percy_snapshot(browser, "Blog List Page")
 
     # blog post
-    browser.get(
-        "http://localhost:8000/updates/2021/06/09/making-progress-on-the-cool-project/"
-    )
+    browser.get("http://localhost:8000/updates/2021/07/06/a-big-announcement/")
+    browser.implicitly_wait(10)
     percy_snapshot(browser, "Blog Post")
+
+    # 404 page
+    browser.get("http://localhost:8000/bad-url")
+    browser.implicitly_wait(10)
+    percy_snapshot(browser, "404 Page")
 
     # shut down when finished
     browser.quit()
