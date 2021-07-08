91d096f8e6857773e3a708e77aad3790e752c4b1
Nick Budak <nbudak@princeton.edu>
2021-07-08 10:54:56 -0400

    Revise visual test script to capture some UX

--- ci/visual_tests.py
+++ ci/visual_tests.py
@@ -3,19 +3,11 @@
 
 """Execute visual regression tests against a running django server."""
 
-# much of this code was adapted from the following example:
-# https://github.com/percy/example-percy-python-selenium/blob/master/tests/todo.py
-
-from typing import Iterable
-
-from percy import percySnapshot
+from percy import percy_snapshot
 from selenium import webdriver
 
-__author__ = "CDH @ Princeton"
-__email__ = "cdh-info@princeton.edu"
-
-# viewport widths at which to take a snapshot of a page
-DEVICE_WIDTHS = [375, 768, 1280]
+# For docs on Percy's python/selenium integration, see:
+# https://docs.percy.io/docs/python-selenium
 
 
 def setup() -> webdriver.Chrome:
@@ -29,34 +21,59 @@ def setup() -> webdriver.Chrome:
     return webdriver.Chrome(options=options)
 
 
-def get_urls() -> Iterable[str]:
-    """Get the set of URLs to execute visual regression testing against."""
-    # TODO make this smarter; maybe crawl the top-level sitemap?
-    return [
-        "http://localhost:8000/",
-        "http://localhost:8000/about",
-        "http://localhost:8000/grants",
-        "http://localhost:8000/people/staff",
-        "http://localhost:8000/people/meredith-martin/"
-        "http://localhost:8000/projects",
-        "http://localhost:8000/projects/princeton-prosody-archive/"
-        "http://localhost:8000/events",
-        "http://localhost:8000/events/2018/03/dataviz-2/"
-        "http://localhost:8000/updates",
-        "http://localhost:8000/updates/2018/08/28/bridging-dh-oss/"
-    ]
-
-
-def run() -> None:
+def run():
     """Take DOM snapshots of a set of URLs and upload to Percy."""
     browser = setup()
 
-    # visit each URL, wait 10 seconds, then upload to Percy
-    for url in get_urls():
-        browser.get(url)
-        browser.implicitly_wait(10)
-        percySnapshot(browser=browser, name=browser.title,
-                      widths=DEVICE_WIDTHS)
+    # homepage
+    browser.get("http://localhost:8000/")
+    percy_snapshot(browser, "Home")
+
+    # landing page
+    browser.get("http://localhost:8000/landing")
+    percy_snapshot(browser, "Landing Page")
+
+    # content page
+    browser.get("http://localhost:8000/content")
+    percy_snapshot(browser, "Content Page")
+
+    # projects list page
+    browser.get("http://localhost:8000/projects/sponsored")
+    browser.find_element_by_class_name("toggle").click()  # expand dropdown
+    percy_snapshot(browser, "Projects List Page")
+
+    # project page
+    browser.get("http://localhost:8000/projects/derridas-margins/")
+    percy_snapshot(browser, "Project Page")
+
+    # people list page
+    browser.get("http://localhost:8000/people/staff")
+    browser.find_element_by_class_name("toggle").click()
+    percy_snapshot(browser, "Person List Page")
+
+    # profile page
+    browser.get("http://localhost:8000/people/staffer/")
+    percy_snapshot(browser, "Profile Page")
+
+    # events list page
+    browser.get("http://localhost:8000/events")
+    browser.find_element_by_class_name("toggle").click()
+    percy_snapshot(browser, "Events List Page")
+
+    # event page
+    browser.get("http://localhost:8000/events/2017/02/testing-course/")
+    percy_snapshot(browser, "Event Page")
+
+    # blog list page
+    browser.get("http://localhost:8000/updates")
+    browser.find_element_by_class_name("toggle").click()
+    percy_snapshot(browser, "Blog List Page")
+
+    # blog post
+    browser.get(
+        "http://localhost:8000/updates/2021/06/09/making-progress-on-the-cool-project/"
+    )
+    percy_snapshot(browser, "Blog Post")
 
     # shut down when finished
     browser.quit()
