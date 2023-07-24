f23eddde5aa0766844a449e3bde8501a43d676e4
rlskoeser <rebecca.s.koeser@princeton.edu>
2023-07-24 16:29:05 -0400

    Update to python 3.9 and django 4.0

--- README.rst
+++ README.rst
@@ -31,7 +31,7 @@ CDH Website
     :target: https://pycqa.github.io/isort/
     :alt: "imports: isort"
 
-Python 3.8 / Django 3.2 / Node 10 / PostgreSQL 12
+Python 3.9 / Django 4.0 / Node 10 / PostgreSQL 12
 `cdhweb` is a Django+Mezzanine application that powers the CDH website
 with custom models for people, events, and projects.
 
--- cdhweb/blog/models.py
+++ cdhweb/blog/models.py
@@ -3,7 +3,7 @@ from django.db.models.fields.related import RelatedField
 from django.urls import reverse
 from django.utils.dateformat import format
 from django.utils.text import Truncator
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from modelcluster.fields import ParentalKey
 from modelcluster.models import ClusterableModel
