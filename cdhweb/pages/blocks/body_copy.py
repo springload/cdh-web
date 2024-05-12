77ae3477d71a85416420df2ead485157f9d7a6d2
sarahframe <sarah.frame@springload.co.nz>
2024-05-13 09:15:10 +1200

    remove files not ready yet

--- cdhweb/pages/blocks/body_copy.py
+++
@@ -1,9 +0,0 @@
-from wagtail import blocks
-
-
-class BodyCopy(blocks.StructBlock):
-    class Meta:
-        template = "core/blocks/body_copy.html"
-        label = "Body Copy"
-
-    # body - add a block of text to a page. Rich text with blocks
