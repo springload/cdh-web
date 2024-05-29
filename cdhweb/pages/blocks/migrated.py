090305b5272056e815ea2e474cc766a3f7e05f74
sarahframe <sarah.frame@springload.co.nz>
2024-05-30 10:31:09 +1200

    fixing migrate block

--- cdhweb/pages/blocks/migrated.py
+++ cdhweb/pages/blocks/migrated.py
@@ -2,15 +2,17 @@ from wagtail import blocks
 
 
 class MigratedBlock(blocks.RichTextBlock):
-    class Meta:
-        group = "Deprecated"
-        icon = "warning"
-
     #: used to hold content migrated from mezzanine via a "kitchen-sink"
     #: approach; enable all supported wagtail features.
     #: Should NOT be used when creating new pages.
-    migrated = blocks.RichTextBlock(
-        features=[
+
+    class Meta:
+        icon = "warning"
+        template = "text-content.html"
+        group = "Deprecated"
+
+    def __init__(self, *args, **kwargs):
+        kwargs["features"] = [
             "h3",
             "h4",
             "bold",
@@ -26,7 +28,5 @@ class MigratedBlock(blocks.RichTextBlock):
             "code",
             "image",
             "embed",
-        ],
-        icon="warning",
-        template="text-content.html",
-    )
+        ]
+        super().__init__(*args, **kwargs)
