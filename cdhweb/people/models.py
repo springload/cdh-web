e44a7d1b0d73416c91e36c7d7a99711b7f25b333
sarahframe <sarah.frame@springload.co.nz>
2024-06-04 12:07:07 +1200

    dont make image mandatory - issues with migration of models with no image

--- cdhweb/people/models.py
+++ cdhweb/people/models.py
@@ -312,7 +312,7 @@ class Person(ClusterableModel):
     image = models.ForeignKey(
         "wagtailimages.image",
         null=True,
-        blank=False,
+        blank=True,
         on_delete=models.SET_NULL,
         related_name="+",
     )  # no reverse relationship
